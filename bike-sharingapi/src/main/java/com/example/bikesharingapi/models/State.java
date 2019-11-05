6f759376db7868a62929cef0879d2d055ddbb3c6
Marțincu Petru <martincu.petru@gmail.com>
2019-11-05 15:42:31 +0200

    Added state model annotations

--- bike-sharingapi/src/main/java/com/example/bikesharingapi/models/State.java
+++ bike-sharingapi/src/main/java/com/example/bikesharingapi/models/State.java
@@ -1,38 +1,44 @@
 package com.example.bikesharingapi.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity(name = "state")
 public class State {
-    int id;
-    int name;
-    int description;
 
+    @Id
+    private UUID stateId;
+    private String name;
+    private String description;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getName() {
-        return name;
-    }
-
-    public void setName(int name) {
+    public State(UUID stateId, String name, String description) {
+        this.stateId = stateId;
         this.name = name;
-    }
-
-    public int getDescription() {
-        return description;
-    }
-
-    public void setDescription(int description) {
         this.description = description;
     }
 
-    public State(int id, int name, int description) {
-        this.id = id;
+    public UUID getStateId() {
+        return stateId;
+    }
+
+    public void setStateId(UUID stateId) {
+        this.stateId = stateId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 }
