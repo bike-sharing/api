0b8c8d3b27b2a238ef6ce0bccfd4a277518ad459
Marțincu Petru <martincu.petru@gmail.com>
2019-11-05 16:30:41 +0200

    Added get mappings bicycles

--- bike-sharingapi/src/main/java/com/example/bikesharingapi/controllers/BicycleController.java
+++ bike-sharingapi/src/main/java/com/example/bikesharingapi/controllers/BicycleController.java
@@ -2,22 +2,31 @@ package com.example.bikesharingapi.controllers;
 
 import com.example.bikesharingapi.models.Bicycle;
 import com.example.bikesharingapi.repository.BicycleRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 public class BicycleController {
+
+    @Autowired
     private BicycleRepository bicycleRepository;
 
-    public BicycleController(){
-        bicycleRepository = new BicycleRepository();
-    }
-
-    @RequestMapping("/bicycle/get")
-    public ArrayList<Bicycle> getAll() {
+    @GetMapping("/bicycles")
+    public List<Bicycle> getAll() {
         return bicycleRepository.getAll();
     }
 
+    @GetMapping("/bicycles/available")
+    public List<Bicycle> getAllAvailable() {
+        return bicycleRepository.getAllByAvailabilityIsTrue();
+    }
+
+    @GetMapping("/bicycles/unavailable")
+    public List<Bicycle> getAllUnavailable() {
+        return bicycleRepository.getAllByAvailabilityIsFalse();
+    }
 }
