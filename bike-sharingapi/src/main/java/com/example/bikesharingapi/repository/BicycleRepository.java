2071fd7f001d2a25bbfe1b9ccc4387a166968ac2
Marțincu Petru <martincu.petru@gmail.com>
2019-11-05 17:14:55 +0200

    Updated repositories with correct methods

--- bike-sharingapi/src/main/java/com/example/bikesharingapi/repository/BicycleRepository.java
+++ bike-sharingapi/src/main/java/com/example/bikesharingapi/repository/BicycleRepository.java
@@ -2,15 +2,14 @@ package com.example.bikesharingapi.repository;
 
 import com.example.bikesharingapi.models.Bicycle;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface BicycleRepository extends JpaRepository<Bicycle, UUID> {
 
-    @Query("select b from bicycle b")
-    List<Bicycle> getAll();
-    List<Bicycle> getAllByAvailabilityIsTrue();
-    List<Bicycle> getAllByAvailabilityIsFalse();
+    List<Bicycle> getAllBy();
+    Bicycle getByBicycleId(UUID bicycleId);
+    boolean deleteByBicycleIdIs(UUID bicycleId);
+    void deleteAllBy();
 }
--- bike-sharingapi/src/main/java/com/example/bikesharingapi/repository/LocationRepository.java
+++ bike-sharingapi/src/main/java/com/example/bikesharingapi/repository/LocationRepository.java
@@ -4,8 +4,13 @@ import com.example.bikesharingapi.models.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public interface LocationRepository extends JpaRepository<Location, UUID> {
 
+    List<Location> getAllBy();
+    List<Location> getByLocationId(UUID locationId);
+    void deleteAllBy();
+    void deleteByLocationId(UUID locationId);
 }
