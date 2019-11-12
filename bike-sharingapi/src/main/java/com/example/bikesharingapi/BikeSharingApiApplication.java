7add84f4d661b059a1d4fb53bf780710b4231bfe
Petru <martincu.petru@gmail.com>
2019-11-12 23:00:30 +0200

    Initialised the SDK

--- bike-sharingapi/src/main/java/com/example/bikesharingapi/BikeSharingApiApplication.java
+++ bike-sharingapi/src/main/java/com/example/bikesharingapi/BikeSharingApiApplication.java
@@ -1,13 +1,32 @@
 package com.example.bikesharingapi;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class BikeSharingApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BikeSharingApiApplication.class, args);
+
+        try {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.getApplicationDefault())
+                    .setDatabaseUrl("https://bicyclesharing-4eafb.firebaseio.com")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+
+        } catch (IOException exception) {
+            System.out.println("Error occured an initialising Firebase Admin SDK: " + exception.getMessage());
+        }
+
+
     }
 
 }
