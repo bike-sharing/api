0d44de5f4a4fcbcec54202950bb236b8dfb01f9a
Marțincu Petru <martincu.petru@gmail.com>
2019-12-17 17:42:04 +0200

    Added missing path from QR code generation

--- bike-sharingapi/src/main/java/com/example/bikesharingapi/utils/QRcodeGenerator.java
+++ bike-sharingapi/src/main/java/com/example/bikesharingapi/utils/QRcodeGenerator.java
@@ -4,6 +4,6 @@ import net.glxn.qrgen.javase.QRCode;
 
 public class QRcodeGenerator {
     public static byte[] GetQRcodeBytestream(String id, String server) {
-        return QRCode.from("https://" + server + "/" + id + "/borrow").stream().toByteArray();
+        return QRCode.from("https://" + server + "/bicycle/" + id + "/borrow").stream().toByteArray();
     }
 }
