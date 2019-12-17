4ad696f11be69ef4e7ec14c73477b9c8ae70d9e7
Marțincu Petru <martincu.petru@gmail.com>
2019-12-17 15:05:46 +0200

    Modified QR code generator to return byte array

--- bike-sharingapi/src/main/java/com/example/bikesharingapi/utils/QRcodeGenerator.java
+++ bike-sharingapi/src/main/java/com/example/bikesharingapi/utils/QRcodeGenerator.java
@@ -1,14 +1,10 @@
 package com.example.bikesharingapi.utils;
 
 import net.glxn.qrgen.javase.QRCode;
-
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
 import java.util.UUID;
 
 public class QRcodeGenerator {
-    public static String GetQRcodeBytestream(UUID id) {
-        return QRCode.from(id.toString()).stream().toString();
+    public static byte[] GetQRcodeBytestream(String id) {
+        return QRCode.from(id).stream().toByteArray();
     }
 }
