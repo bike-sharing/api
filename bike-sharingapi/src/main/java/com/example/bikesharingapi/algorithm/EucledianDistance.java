263c60cbd2ff96f00da181f68834e3613e86185e
Petru <martincu.petru@gmail.com>
2020-01-13 21:32:37 +0200

    Updated class implementing old interface with altitude

--- bike-sharingapi/src/main/java/com/example/bikesharingapi/algorithm/EucledianDistance.java
+++ bike-sharingapi/src/main/java/com/example/bikesharingapi/algorithm/EucledianDistance.java
@@ -1,21 +1,22 @@
 package com.example.bikesharingapi.algorithm;
 
+import com.example.bikesharingapi.models.Coordinates;
+
 public class EucledianDistance implements Distance {
     @Override
-    public double calculate(double latitude1, double latitude2, double longitude1, double longitude2, double altitude1, double altitude2) {
+    public double calculate(Coordinates a, Coordinates b) {
         final int R = 6371; // Radius of the earth
 
-        double latDistance = Math.toRadians(latitude2 - latitude1);
-        double lonDistance = Math.toRadians(longitude2 - longitude1);
-        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-                + Math.cos(Math.toRadians(latitude1)) * Math.cos(Math.toRadians(latitude2))
+        double latDistance = Math.toRadians(a.getLatitude() - b.getLatitude());
+        double lonDistance = Math.toRadians(a.getLongitude() - b.getLongitude());
+        double x = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(a.getLatitude())) * Math.cos(Math.toRadians(b.getLatitude()))
                 * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double c = 2 * Math.atan2(Math.sqrt(x), Math.sqrt(1 - x));
         double distance = R * c * 1000; // convert to meters
 
-        double height = altitude1 - altitude2;
 
-        distance = Math.pow(distance, 2) + Math.pow(height, 2);
+        distance = Math.pow(distance, 2);
 
         return Math.sqrt(distance);
     }
